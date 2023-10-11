ea373a3363378f1c548cef030e61fe79cf4bd051
Harsh <hparashar617@gmail.com>
2023-10-11 15:36:15 +0530

    setting up clerk setup

---
+++ app/(Dashboard)/(Routes)/Dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Dashboard = ( ) => {
+  return (
+    <>
+    <p> Dashboard !</p>
+    </>
+  )
+}
+
+export default Dashboard;
---
+++ app/Landing/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Landing = ( ) => {
+  return (
+    <>
+    <p>Landing !</p>
+    </>
+  )
+}
+
+export default Landing;
